79eca7e0e30d1be7d78738eca1d5b6f722d1b1e1
yangqiong <yangqionggo@163.com>
2022-07-23 08:59:49 +0800

    e104_maximum_depth_of_binary_tree

---
+++ javascript/src/e104_maximum_depth_of_binary_tree.js
@@ -0,0 +1,22 @@
+// 104. 二叉树的最大深度
+// https://leetcode.cn/problems/maximum-depth-of-binary-tree/'
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root) {
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  } else {
+    return 0;
+  }
+};
